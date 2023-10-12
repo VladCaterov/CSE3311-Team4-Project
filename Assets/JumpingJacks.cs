7dd0d30dbe4a1c7c0c0f933262bdadd618504623
ElisabethHarrisUTA <123337168+ElisabethHarrisUTA@users.noreply.github.com>
2023-10-12 18:05:52 -0500

    added Inventory Buttom

--- Assets/JumpingJacks.cs
+++ Assets/JumpingJacks.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -35,4 +35,4 @@ public class JumpingJacks : MonoBehaviour
     {
         SwapSprite();
     }
-}
+}*/
