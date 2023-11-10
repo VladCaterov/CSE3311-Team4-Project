983b15fd636143047a7e9c25b4389a368f380afd
Vladimir Caterov <vladcaterovdev@gmail.com>
2023-11-10 17:49:39 -0600

    updated game manager

--- Assets/Scripts/Sounds/GameAudioManager.cs
+++ Assets/Scripts/Sounds/GameAudioManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameAudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameAudioManager instance;
+    public Sound[] sounds;
+    void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    private void Start()
     {
-        
+        Play("Main");
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.soundName == name);
+        s.source.Play();
     }
 }
--- Assets/Scripts/Sounds/Sound.cs
+++ Assets/Scripts/Sounds/Sound.cs
@@ -4,7 +4,7 @@ using UnityEngine.Audio;
 using UnityEngine;
 
 
-[System.Serializable]
+[System.Serializable] //Allows Unity to use this class
 public class Sound
 {
     // Start is called before the first frame update
@@ -16,5 +16,6 @@ public class Sound
     [Range(0.1f, 3f)]
     public float pitch;
 
+    [HideInInspector]
     public AudioSource source;
 }
