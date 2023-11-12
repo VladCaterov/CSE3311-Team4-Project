6469e88feead0453ab385fd2a0ed22d86b4501df
Vladimir Caterov <vladcaterovdev@gmail.com>
2023-11-11 19:12:00 -0600

    attempting to fix audio

---
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        foreach (Sound s in sounds)
+        {
+           s.source =  gameObject.AddComponent<AudioSource>();
+           s.source.clip = s.clip;
+           s.source.volume = s.volume;
+           s.source.pitch = s.pitch;
+           s.source.loop = s.loop;
+        }
+    }
+    void Start()
+    {
+        Play("Main Audio");
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
--- Assets/Scripts/Sounds/Sound.cs
+++ Assets/Scripts/Sounds/Sound.cs
@@ -4,18 +4,18 @@ using UnityEngine.Audio;
 using UnityEngine;
 
 
-[System.Serializable] //Allows Unity to use this class
+[System.Serializable]
 public class Sound
 {
     // Start is called before the first frame update
-    public string soundName;
+    public string name;
     public AudioClip clip;
 
     [Range(0f, 1f)]
     public float volume;
     [Range(0.1f, 3f)]
     public float pitch;
-
+    public bool loop;
     [HideInInspector]
     public AudioSource source;
 }
