c7c6b9284a146e6ecb02378e5360cf247960fc71
ElisabethHarrisUTA <123337168+ElisabethHarrisUTA@users.noreply.github.com>
2023-11-16 16:37:51 -0600

    Testing Inventory Stuff

---
+++ Assets/Scripts/Inventory Scripts/InventorySystem.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+using static UnityEngine.ParticleSystem;
+
+[System.Serializable]
+public class InventorySystem
+{
+    [SerializeField] private List<InventorySlot> inventorySlots;
+
+
+    public List<InventorySlot> InventorySlots => inventorySlots;
+    public int InventorySize => inventorySlots.Count;
+
+    public UnityAction<InventorySlot> OnInventorySlotChanged;
+
+    public InventorySystem(int size)
+    {
+        inventorySlots = new List<InventorySlot>(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            inventorySlots.Add(new InventorySlot());
+        }
+    }
+
+    public bool AddToInventory(InventoryItemData itemToAdd, int amountToAdd)
+    {
+        if ((ContainsItem(itemToAdd, out List<InventorySlot> invSlot)) // Check whether item exists in inventory
+        {
+            foreach(var slot in invSlot)
+            {
+                if(slot.RoomLeftInStack(amountToAdd))
+                {
+                    slot.AddToStack(amountToAdd);
+                    OnInventorySlotChanged?.Invoke(slot);
+                    return true;
+
+                }
+            }
+        }
+        
+        if (HasFreeSlot(out InventorySlot freeSlot)) // Gets the first available slot
+        {
+            freeSlot.UpdateInventorySlot(itemToAdd, amountToAdd);
+            OnInventorySlotChanged?.Invoke(freeSlot);
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool ContainsItem(InventoryItemData itemToAdd, out List<InventorySlot> invSlot)
+    {
+        invSlot = InventorySlots.Where(i => i.ItemData == itemToAdd).ToList(); // creates a list of inventory slots where the item data is equal to the item we want to add
+        return invSlot == null ? false : true;
+    }
+
+    public bool HasFreeSlot(out InventorySlot freeSlot)
+    {
+        freeSlot = InventorySlots.FirstOrDefault(i => i.ItemData == null); ;
+        return freeSlot == null ? false : true;
+    }
+}
