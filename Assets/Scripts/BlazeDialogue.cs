5668c1f38fd608ba63cdd6a24e8ac94665605a98
Juan Hernandez <jh183961@gmail.com>
2023-11-21 22:30:59 -0600

    Picket System Complete. Ready for integration with the map!!

--- Assets/Scripts/BlazeDialogue.cs
+++ Assets/Scripts/BlazeDialogue.cs
@@ -6,16 +6,6 @@ using UnityEngine.UI;
 [System.Serializable]
 public class BlazeDialogue
 {
-    /*
-    public Text Dorm1Info;
-    public Text Dorm2Info;
-    public Text Dorm3Info;
-    public Text Dorm4Info;
-    public Text LibraryInfo;
-    public Text PickardHallInfo;
-    public Text ChemBuilding;
-    public Text PhysBuilding;
-    */
     public Animator BlazeTalk;
 
     public string name;
